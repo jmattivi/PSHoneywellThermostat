5c4dba6f2eebbd4e5b439a18920bb4551440747d
Jon Mattivi <jmattivi@gmail.com>
2025-10-29 10:34:38 -0400

    Update Get-OutsideTemperature.ps1

--- PSHoneywellThermostat/Public/Get-OutsideTemperature.ps1
+++ PSHoneywellThermostat/Public/Get-OutsideTemperature.ps1
@@ -7,53 +7,24 @@ function Get-OutsideTemperature
     	CREATED DATE: 2017-12-04
 
     .SYNOPSIS
-        Integrates with the Accuweather API to query the current temperature.
+        Integrates with the weather.gov API to query the current temperature.
         
     .DESCRIPTION
-        API key is hardcoded in the parameter
-        Default location is set for Danville,PA
+        API for weather.gov uses latitude and longitude hardcoded for Danville, PA
 
-    .PARAMETER apikey
-
-    .PARAMETER location
-    	    
     .EXAMPLE
     	Get-OutsideTemperature
 
     #>
     [CmdletBinding()]
     Param (
-        [parameter(position = 1, mandatory = $false)]    
-        [String]$apikey = "",
-        [parameter(position = 2, mandatory = $false)]
-        [ValidateSet('Danville,PA')]
-        [String]$location = 'Danville,PA'
     )
 
-    SWITCH ($location)
-    {
-        'Danville,PA'
-        {
-            $locationid = "335323"
-        }
-    }
+    #$url = "https://api.weather.gov/points/40.96342,-76.6127329"
+    $url = "https://api.weather.gov/gridpoints/CTP/111,70/forecast/hourly"
+    $result = Invoke-RestMethod -Method Get -Uri $url
 
-    $arrtemp = @()
+    $currenttemp = ($result.properties.periods | Select -First 1).temperature
 
-    $url = "http://dataservice.accuweather.com/currentconditions/v1/$locationid`?apikey=$apikey&details=True"
-    $tempresult = Invoke-RestMethod -Uri $url -Method Get
-    
-    $currenttemp = $tempresult.Temperature.Imperial.Value
-    $objzone = new-object psobject
-    $objzone | Add-Member -MemberType NoteProperty -Name 'TempDescription' -Value "Current"
-    $objzone | Add-Member -MemberType NoteProperty -Name 'TempValue' -Value $currenttemp
-    $arrtemp += $objzone
-
-    $realfeel = $tempresult.RealFeelTemperature.Imperial.Value
-    $objzone = new-object psobject
-    $objzone | Add-Member -MemberType NoteProperty -Name 'TempDescription' -Value "RealFeel"
-    $objzone | Add-Member -MemberType NoteProperty -Name 'TempValue' -Value $realfeel
-    $arrtemp += $objzone
-
-    Write-Output $arrtemp
+    return $currenttemp
 }
